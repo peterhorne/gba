97ca6553c1d32fa2e7ec82e27f24d71c295a3aec
Peter Horne <peterhorne92@gmail.com>
2016-10-10 21:40:03 +0100

    Test ROM

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ mod bit;
 mod cpu;
 
 use cpu::Cpu;
+use std::fs::File;
+use std::io::*;
 
 fn main() {
-    let cpu = Cpu::new();
+    let file = File::open("./misc/super-mario.gba").unwrap();
+    let mut rom = BufReader::new(file);
+    let mut buffer = [0; 4];
 
-    // fetch
+    let mut cpu = Cpu::new();
 
-    // decode
-
-    // execute
+    loop {
+        rom.seek(SeekFrom::Start(cpu.pc() as u64)).unwrap();
+        rom.read_exact(&mut buffer);
+        let instruction = buffer[0] as u32
+                        + ((buffer[1] as u32) << 8)
+                        + ((buffer[2] as u32) << 16)
+                        + ((buffer[3] as u32) << 24);
+        println!("{:b}", instruction);
+        cpu.execute(instruction);
+    }
 }
