e8c76819fd330b666dc385db633dddcb216230aa
Peter Horne <peterhorne92@gmail.com>
2016-10-23 21:56:32 +0100

    AND instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -375,6 +375,17 @@ impl Cpu {
 
     fn and(&mut self, s: bool, rd: Register, rn: Register, operand2: (u32, bool)) {
         println!("Instruction: and");
+        let (shifter_operand, shifter_carry_out) = operand2;
+        let result = self.registers[rn] & shifter_operand;
+        self.registers[rd] = result;
+
+        if s && rd == Register(15) {
+            self.cpsr = self.spsr;
+        } else if s {
+            self.set_n(result.bit(31));
+            self.set_z(result == 0);
+            self.set_c(shifter_carry_out);
+        }
     }
 
     fn b(&mut self, l: bool, signed_immed: u32) {
