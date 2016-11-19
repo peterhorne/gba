a2a0962c8c2a6c6a77923dcee24b78d45f594049
Peter Horne <peterhorne92@gmail.com>
2016-11-19 09:58:59 +0000

    Remove instruction logging

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,12 @@ fn main() {
     loop {
         rom.seek(SeekFrom::Start(cpu.pc() as u64)).unwrap();
         rom.read_exact(&mut buffer);
+
         let instruction = buffer[0] as u32
                         + ((buffer[1] as u32) << 8)
                         + ((buffer[2] as u32) << 16)
                         + ((buffer[3] as u32) << 24);
-        println!("{:b}", instruction);
+
         cpu.execute(instruction);
     }
 }
