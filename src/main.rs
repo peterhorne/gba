23bbf04daf209431321445cdc8db4c75f3e2fe2d
Peter Horne <peterhorne92@gmail.com>
2018-02-28 22:46:25 +0000

    Read paths for bios/rom from stdin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 extern crate byteorder;
 extern crate core;
+#[macro_use]
+extern crate structopt;
 
 mod bit;
 mod bus;
@@ -18,12 +20,38 @@ use memory_map::MemoryMap;
 use std::cell::RefCell;
 use std::fs::File;
 use std::io::*;
+use std::path::PathBuf;
+use std::process;
 use std::rc::Rc;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "GBA Emulator")]
+struct Opt {
+    /// Path to ROM
+    #[structopt(short = "r", long = "rom", parse(from_os_str))]
+    rom: PathBuf,
+
+    /// Path to BIOS
+    #[structopt(short = "b", long = "bios", parse(from_os_str))]
+    bios: PathBuf,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+    match run(opt) {
+        Ok(_) => process::exit(0),
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+}
+
+fn run(opt: Opt) -> std::result::Result<(), String> {
     let interrupts = Rc::new(RefCell::new(InterruptController::new()));
-    let bios = BufReader::new(File::open("./misc/bios-dump.bin").unwrap());
-    let rom = BufReader::new(File::open("./misc/super-mario.gba").unwrap());
+    let bios = BufReader::new(File::open(opt.bios).map_err(|err| format!("Error reading BIOS:\n  {}", err))?);
+    let rom = BufReader::new(File::open(opt.rom).map_err(|err| format!("Error reading ROM:\n  {}", err))?);
     let memory = MemoryMap::new(bios, rom, Rc::clone(&interrupts));
     let mut cpu = Cpu::new(memory, Rc::clone(&interrupts));
     loop { cpu.tick(); }
